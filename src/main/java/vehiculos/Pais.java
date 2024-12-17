ddc33297486f144fc43a7f5dbb2800d215655c59
LuisRincon16 <lurinconj@unal.edu.co>
2024-12-17 11:24:58 +0000

    int10

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -17,6 +17,20 @@ public class Pais{
         this.contadorP += 1;
     }
 
+    public static Pais paisMasVendedor(){
+        int maxNumPaises = 0;
+        Pais paisMasVen = null;
+        
+        for (Fabricante i : Fabricantes){
+            if (i.getPais().getContadorP() >= maxNumPaises){
+                maxNumPaises = i.getPais().getContadorP();
+                paisMasVen = i.getPais();
+            }
+        }
+        
+        return paisMasVen;
+    }
+
 
     public Pais(String nom){
         this.nombre = nom;
