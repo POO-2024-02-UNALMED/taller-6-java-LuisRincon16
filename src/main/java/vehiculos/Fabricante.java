6996dc773c3dea2159a99537e59a9d3045897c40
LuisRincon16 <lurinconj@unal.edu.co>
2024-12-17 10:05:35 +0000

    int7

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,12 +1,27 @@
 package vehiculos;
+import java.util.ArrayList;
 
 public class Fabricante {
     private String nombre;
     private Pais pais;
+    private static ArrayList<Fabricante> listadoF = new ArrayList<>();
+    private int contadorF;
 
     public Fabricante(String nom, Pais pais){
         this.nombre = nom;
         this.pais = pais;
+        Fabricante.listadoF.add(this);
+    }
+
+    public static void setListadoF(ArrayList<Fabricante> list){
+        Fabricante.listadoF = list;
+    }
+    public static ArrayList<Fabricante> getListadoF(){
+        return Fabricante.listadoF;
+    }
+
+    public int getContadorF(){
+        return this.contadorF;
     }
 
     public void setNombre(String nom){
